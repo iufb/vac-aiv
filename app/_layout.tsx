5b2f401cc09c121b192d78bdc8c69fff5716a757
iufb <bashirov3ld2@gmail.com>
2025-12-28 17:00:07 +0500

    Refactor app configuration and improve tab navigation layout
    - Updated app.json for Android package name change and formatting adjustments.
    - Removed unused dependency from package-lock.json.
    - Enhanced RootLayout component with backBehavior and animation settings.
    - Improved StackScreen component's back button functionality and layout.
    - Refined Tabbar component styles and structure for better responsiveness.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,15 @@ import { Tabs } from "expo-router";
 import { Tabbar } from "~/components/tabbar";
 import "~/i18n";
 export default function RootLayout() {
-    return <Tabs
-        screenOptions={{
-            animation: 'fade'
-        }}
-        tabBar={(props) => <Tabbar {...props} />}
+  return (
+    <Tabs
+      screenOptions={{
+        animation: "none",
+      }}
+      backBehavior="history"
+      tabBar={(props) => <Tabbar {...props} />}
     >
-        <Tabs.Screen name="home.Abbr" />
+      <Tabs.Screen name="home.Abbr" />
     </Tabs>
+  );
 }
--- components/StackScreen.tsx
+++ components/StackScreen.tsx
@@ -5,39 +5,40 @@ import { isTablet } from "~/components/constants";
 import ChevronLeft from "../assets/icons/chevron-left.svg";
 
 export const StackScreen = ({
-    title,
-    showBackBtn = true,
+  title,
+  showBackBtn = true,
 }: {
-    title: string;
-    showBackBtn?: boolean;
+  title: string;
+  showBackBtn?: boolean;
 }) => {
-    const router = useRouter();
-    return (
-        <Stack.Screen
-            options={{
-                headerTitleAlign: "center",
-                headerTitle: () => (
-                    <Text style={{ fontSize: isTablet ? 24 : 18 }} numberOfLines={1}>
-                        {title}
-                    </Text>
-                ),
-                headerLeft: showBackBtn
-                    ? () => (
-                        <TouchableOpacity
-                            style={{
-                                alignItems: "center",
-                                paddingHorizontal: 4,
-                                justifyContent: "center",
-                                marginHorizontal: 5,
-                            }}
-                            onPress={() => router.back()}
-                        >
-                            <ChevronLeft width={24} height={24} />
-                        </TouchableOpacity>
-                    )
-                    : undefined,
-                headerBackVisible: false,
-            }}
-        />
-    );
+  const router = useRouter();
+  return (
+    <Stack.Screen
+      options={{
+        headerTitleAlign: "center",
+        headerTitle: () => (
+          <Text style={{ fontSize: isTablet ? 24 : 18 }} numberOfLines={1}>
+            {title}
+          </Text>
+        ),
+        headerLeft: showBackBtn
+          ? () => (
+              <TouchableOpacity
+                hitSlop={20}
+                style={{
+                  alignItems: "center",
+                  paddingHorizontal: 4,
+                  justifyContent: "center",
+                  marginTop: 4,
+                }}
+                onPress={() => router.back()}
+              >
+                <ChevronLeft width={24} height={24} />
+              </TouchableOpacity>
+            )
+          : undefined,
+        headerBackVisible: false,
+      }}
+    />
+  );
 };
