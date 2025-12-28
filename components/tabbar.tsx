67fbe0e39bb239422161bd8534fbf0195627c013
iufb <bashirov3ld2@gmail.com>
2025-12-28 17:00:07 +0500

    Refactor app configuration and improve tab navigation layout
    - Updated app.json for Android package name change and formatting adjustments.
    - Removed unused dependency from package-lock.json.
    - Enhanced RootLayout component with backBehavior and animation settings.
    - Improved StackScreen component's back button functionality and layout.
    - Refined Tabbar component styles and structure for better responsiveness.

--- components/tabbar.tsx
+++ components/tabbar.tsx
@@ -2,104 +2,102 @@ import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
 import { Link, LinkProps, usePathname } from "expo-router";
 import { ReactElement, ReactNode, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
-import Animated, { interpolateColor, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { tabColors } from "~/components/consts/colors";
 import { BookIcon, HomeIcon, SettingsIcon } from "~/components/icons";
 
+const tabs: {
+  label: string;
+  href: LinkProps["href"];
+  icon: (color: string) => ReactElement;
+}[] = [
+  {
+    label: "Home",
+    href: "/",
+    icon: (color) => <HomeIcon color={color} width={24} height={24} />,
+  },
+  {
+    label: "Explore",
 
-const tabs: { label: string, href: LinkProps['href'], icon: (color: string) => ReactElement }[] = [
-    {
-        label: "Home",
-        href: "/",
-        icon: (color) => <HomeIcon color={color} width={24} height={24} />
-    },
-    {
-        label: "Explore",
+    href: "/explore",
 
-        href: "/explore",
+    icon: (color) => <BookIcon color={color} width={24} height={24} />,
+  },
+  {
+    label: "Settings",
 
-        icon: (color) => <BookIcon color={color} width={24} height={24} />
+    href: "/settings",
 
-    },
-    {
-
-        label: "Settings",
-
-        href: "/settings",
-
-        icon: (color) => <SettingsIcon color={color} width={24} height={24} />
-    }
-
-
-
+    icon: (color) => <SettingsIcon color={color} width={24} height={24} />,
+  },
 ];
 function isRouteActive(path: string, target: string) {
-    return path === target || path.startsWith(`${target}/`);
+  return path === target || path.startsWith(`${target}/`);
 }
-interface TabbarProps extends BottomTabBarProps { }
+interface TabbarProps extends BottomTabBarProps {}
 export const Tabbar = ({ ...props }: TabbarProps) => {
-    const { insets } = props
-    console.log(insets)
+  const { insets } = props;
+  console.log(insets);
 
-    return (
-        <View style={[styles.container, { paddingBottom: insets.bottom }]}>
-
-
-            {tabs.map(t => <Tablink t={t} key={t.href.toString()} />)}
-        </View>
-
-    );
-}
+  return (
+    <View style={[styles.container, { paddingBottom: insets.bottom + 10 }]}>
+      {tabs.map((t) => (
+        <Tablink t={t} key={t.href.toString()} />
+      ))}
+    </View>
+  );
+};
 interface TablinkProps {
-    t: typeof tabs[0]
+  t: (typeof tabs)[0];
 }
 
 interface TabIconProps {
-    icon: ReactNode,
-    isActive: boolean
+  icon: ReactNode;
+  isActive: boolean;
 }
 
-
 const Tablink = ({ t }: TablinkProps) => {
-    const progress = useSharedValue(0)
-    const pathname = usePathname();
-    const isActive = isRouteActive(pathname, t.href.toString());
-    useEffect(() => {
-        progress.value = withTiming(isActive ? 1 : 0, { duration: 250 });
-    }, [isActive]);
-    const st = useAnimatedStyle(() => ({
-        transform: [
-            {
-                scale: withTiming(isActive ? 1.1 : 1, { duration: 300 })
+  const progress = useSharedValue(0);
+  const pathname = usePathname();
+  const isActive = isRouteActive(pathname, t.href.toString());
+  useEffect(() => {
+    progress.value = withTiming(isActive ? 1 : 0, { duration: 250 });
+  }, [isActive]);
+  const st = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: withTiming(isActive ? 1.1 : 1, { duration: 300 }),
+      },
+    ],
+    backgroundColor: interpolateColor(
+      progress.value,
+      [1, 0],
+      [tabColors.active.bg, tabColors.inactive.bg]
+    ),
+  }));
 
-            }
-        ],
-        backgroundColor: interpolateColor(progress.value, [1, 0], [tabColors.active.bg, tabColors.inactive.bg]),
-    }))
-
-
-    return (
-        <Link href={t.href} key={t.label}>
-            <Animated.View style={[styles.link, st]}>
-                {t.icon(isActive ? tabColors.active.fg : tabColors.inactive.fg)}
-            </Animated.View>
-        </Link>
-    );
+  return (
+    <Link href={t.href} key={t.label}>
+      <Animated.View style={[styles.link, st]}>
+        {t.icon(isActive ? tabColors.active.fg : tabColors.inactive.fg)}
+      </Animated.View>
+    </Link>
+  );
 };
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: 20,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly'
-    },
-    link: {
-        paddingVertical: 3,
-        paddingHorizontal: 6,
-        borderRadius: 10
-    }
-
-
-
-})
-
-
+  container: {
+    paddingTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  link: {
+    paddingVertical: 3,
+    paddingHorizontal: 6,
+    borderRadius: 10,
+  },
+});
